f64d940d77a6a9fb9663f2ccc9e0e8d1d24e7abe
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:32:00 +0300

    sum

---
+++ proje51.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+    // FUTBOL SKOR PUAN PROGRAMII
+    
+	//int skor[10]={2,1,0,0,2,1,1,2,1,0};
+    //int i,puan=0;    
+    
+    //for(i=0;i<10;i++)
+    //{
+    	//if(skor[i]==2)
+    	//{
+    		//puan=puan+3;
+		//}
+		//if(skor[i]==0)
+		//{
+			//puan=puan+1;
+		//}
+	//}
+
+    //if(puan>=12)
+    //{
+    	//printf("Puaniniz: %d ligde kaldiniz.",puan);
+	//}
+	//else
+	//{
+		//printf("Puaniniz: %d ligden dustunuz.",puan);
+	//}
+	
+	
+	   
+
+    // 1+5+9+13+...+81 dizisinin toplamini bulan program 
+
+	//int i,toplam=0;
+	
+	//for(i=1;i<=81;i+=4)
+	//{
+		//toplam=toplam+i;
+	//}
+	
+	//printf("Toplam: %d",toplam);
+	
+	
+		
+	
+	// Taban Sayisi Girilen Dik Ucgen Programi
+	
+	//int i,j,h;
+	
+	//printf("Taban Sayisini Giriniz: ");
+	//scanf("%d",&h);
+	
+	//for(i=1;i<=h;i++)
+	//{
+		//for(j=1;j<=i;j++)
+		//{
+		   //printf("*");	
+		//}
+		
+		//printf("\n");	
+	//}
+	
+	
+	return 0;
+}
